419c7a99213fdd5265d27c6c360c731dd5e645b3
Linus Karlsson <linus.karlsson@eit.lth.se>
2017-04-20 13:54:23 +0200

    Be consistent with spaces vs. tabs.

--- main.c
+++ main.c
@@ -63,11 +63,11 @@ int main(int argc, char **argv)
 
 	key_to_pem(key, &key_bytes, &key_size);
 	crt_to_pem(crt, &crt_bytes, &crt_size);
-	
+
 	/* Print key and certificate. */
 	print_bytes(key_bytes, key_size);
 	print_bytes(crt_bytes, crt_size);
-	
+
 	/* Free stuff. */
 	EVP_PKEY_free(ca_key);
 	EVP_PKEY_free(key);
@@ -92,12 +92,12 @@ void cleanup_crypto()
 void crt_to_pem(X509 *crt, uint8_t **crt_bytes, size_t *crt_size)
 {
 	/* Convert signed certificate to PEM format. */
-    BIO *bio = BIO_new(BIO_s_mem());
-    PEM_write_bio_X509(bio, crt);
-    *crt_size = BIO_pending(bio);
-    *crt_bytes = (uint8_t *)malloc(*crt_size + 1);
-    BIO_read(bio, *crt_bytes, *crt_size);
-    BIO_free_all(bio);
+	BIO *bio = BIO_new(BIO_s_mem());
+	PEM_write_bio_X509(bio, crt);
+	*crt_size = BIO_pending(bio);
+	*crt_bytes = (uint8_t *)malloc(*crt_size + 1);
+	BIO_read(bio, *crt_bytes, *crt_size);
+	BIO_free_all(bio);
 }
 
 int generate_signed_key_pair(EVP_PKEY *ca_key, X509 *ca_crt, EVP_PKEY **key, X509 **crt)
@@ -112,7 +112,7 @@ int generate_signed_key_pair(EVP_PKEY *ca_key, X509 *ca_crt, EVP_PKEY **key, X50
 	/* Sign with the CA. */
 	*crt = X509_new();
 	if (!*crt) goto err;
-	
+
 	X509_set_version(*crt, 2); /* Set version to X509v3 */
 
 	/* Generate random 20 byte serial. */
@@ -122,8 +122,8 @@ int generate_signed_key_pair(EVP_PKEY *ca_key, X509 *ca_crt, EVP_PKEY **key, X50
 	X509_set_issuer_name(*crt, X509_get_subject_name(ca_crt));
 
 	/* Set validity of certificate to 2 years. */
-    X509_gmtime_adj(X509_get_notBefore(*crt), 0);
-    X509_gmtime_adj(X509_get_notAfter(*crt), (long)2*365*3600);
+	X509_gmtime_adj(X509_get_notBefore(*crt), 0);
+	X509_gmtime_adj(X509_get_notAfter(*crt), (long)2*365*3600);
 
 	/* Get the request's subject and just use it (we don't bother checking it since we generated
 	 * it ourself). Also take the request's public key. */
@@ -147,31 +147,31 @@ err:
 int generate_key_csr(EVP_PKEY **key, X509_REQ **req)
 {
 	*key = EVP_PKEY_new();
-    if (!*key) goto err;
+	if (!*key) goto err;
 	*req = X509_REQ_new();
-    if (!*req) goto err;
+	if (!*req) goto err;
 
-    RSA *rsa = RSA_generate_key(RSA_KEY_BITS, RSA_F4, NULL, NULL);
-    if (!EVP_PKEY_assign_RSA(*key, rsa)) goto err;
+	RSA *rsa = RSA_generate_key(RSA_KEY_BITS, RSA_F4, NULL, NULL);
+	if (!EVP_PKEY_assign_RSA(*key, rsa)) goto err;
 
-    X509_REQ_set_pubkey(*req, *key);
+	X509_REQ_set_pubkey(*req, *key);
 
 	/* Set the DN of the request. */
-    X509_NAME *name = X509_REQ_get_subject_name(*req);
-    X509_NAME_add_entry_by_txt(name, "C", MBSTRING_ASC, (const unsigned char*)REQ_DN_C, -1, -1, 0);
-    X509_NAME_add_entry_by_txt(name, "ST", MBSTRING_ASC, (const unsigned char*)REQ_DN_ST, -1, -1, 0);
-    X509_NAME_add_entry_by_txt(name, "L", MBSTRING_ASC, (const unsigned char*)REQ_DN_L, -1, -1, 0);
-    X509_NAME_add_entry_by_txt(name, "O", MBSTRING_ASC, (const unsigned char*)REQ_DN_O, -1, -1, 0);
-    X509_NAME_add_entry_by_txt(name, "OU", MBSTRING_ASC, (const unsigned char*)REQ_DN_OU, -1, -1, 0);
-    X509_NAME_add_entry_by_txt(name, "CN", MBSTRING_ASC, (const unsigned char*)REQ_DN_CN, -1, -1, 0);
+	X509_NAME *name = X509_REQ_get_subject_name(*req);
+	X509_NAME_add_entry_by_txt(name, "C", MBSTRING_ASC, (const unsigned char*)REQ_DN_C, -1, -1, 0);
+	X509_NAME_add_entry_by_txt(name, "ST", MBSTRING_ASC, (const unsigned char*)REQ_DN_ST, -1, -1, 0);
+	X509_NAME_add_entry_by_txt(name, "L", MBSTRING_ASC, (const unsigned char*)REQ_DN_L, -1, -1, 0);
+	X509_NAME_add_entry_by_txt(name, "O", MBSTRING_ASC, (const unsigned char*)REQ_DN_O, -1, -1, 0);
+	X509_NAME_add_entry_by_txt(name, "OU", MBSTRING_ASC, (const unsigned char*)REQ_DN_OU, -1, -1, 0);
+	X509_NAME_add_entry_by_txt(name, "CN", MBSTRING_ASC, (const unsigned char*)REQ_DN_CN, -1, -1, 0);
 
 	/* Self-sign the request to prove that we posses the key. */
-    if (!X509_REQ_sign(*req, *key, EVP_sha256())) goto err;
-    return 1;
+	if (!X509_REQ_sign(*req, *key, EVP_sha256())) goto err;
+	return 1;
 err:
 	EVP_PKEY_free(*key);
 	X509_REQ_free(*req);
-    return 0;
+	return 0;
 }
 
 int generate_set_random_serial(X509 *crt)
@@ -205,12 +205,12 @@ void initialize_crypto()
 void key_to_pem(EVP_PKEY *key, uint8_t **key_bytes, size_t *key_size)
 {
 	/* Convert private key to PEM format. */
-    BIO *bio = BIO_new(BIO_s_mem());
-    PEM_write_bio_PrivateKey(bio, key, NULL, NULL, 0, NULL, NULL);
-    *key_size = BIO_pending(bio);
-    *key_bytes = (uint8_t *)malloc(*key_size + 1);
-    BIO_read(bio, *key_bytes, *key_size);
-    BIO_free_all(bio);
+	BIO *bio = BIO_new(BIO_s_mem());
+	PEM_write_bio_PrivateKey(bio, key, NULL, NULL, 0, NULL, NULL);
+	*key_size = BIO_pending(bio);
+	*key_bytes = (uint8_t *)malloc(*key_size + 1);
+	BIO_read(bio, *key_bytes, *key_size);
+	BIO_free_all(bio);
 }
 
 int load_ca(const char *ca_key_path, EVP_PKEY **ca_key, const char *ca_crt_path, X509 **ca_crt)
